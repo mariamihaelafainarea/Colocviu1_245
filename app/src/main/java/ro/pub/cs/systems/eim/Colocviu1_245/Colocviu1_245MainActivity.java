fc51aaf975a69552254caeacd84f5e183ea4bca4
mariamihaelafainarea <mariamihaelafainarea4@gmail.com>
2020-04-09 12:56:42 +0300

    renamed some classes

--- app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
+++ app/src/main/java/ro/pub/cs/systems/eim/Colocviu1_245/Colocviu1_245MainActivity.java
@@ -3,12 +3,12 @@ package ro.pub.cs.systems.eim.Colocviu1_245;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Colocviu1_245MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_practical_test01_245_main);
         //
     }
 }
